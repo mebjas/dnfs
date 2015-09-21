d462a517e1695cbba493c7f44d4da73b19abb140
minhaz <minhazav@gmail.com>
2015-09-21 23:35:14 +0530

    Added initial UI and codes

---
+++ dnfs/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dnfs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        // Code to allow drag and drop of the form
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                try
+                {
+                    DragMove();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        // [Event Listener], called when mouseover on close button
+        private void closeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            closeButton.Opacity = 1;
+        }
+
+        // [Event Listener], called when mouseout on close button
+        private void closeButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            closeButton.Opacity = 0.6;
+        }
+
+        private void closeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
